4168923590a12cfcf70ae0a9f061d2bfa504428e
Vyacheslav Krasovskiy <wkrasowsky@gmail.com>
2020-09-06 11:38:27 +0300

    исправлены замечания

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.button__edit');
-const popupCloseButton = document.querySelector('.button__close');
-const popupSaveButton = document.querySelector('.button__submit');
-const nameInput = document.querySelector('.popup__text_name');
-const jobInput = document.querySelector('.popup__text_job');
+const popupOpenButton = document.querySelector('.button_type_edit');
+const popupCloseButton = document.querySelector('.button_type_close');
+const popupSaveButton = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__text_type_name');
+const jobInput = document.querySelector('.popup__text_type_job');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 
@@ -23,18 +23,19 @@ const popupOpen = (event) => {
 }
 
 const popupClose = () => {
-    event.preventDefault();
     popup.classList.remove('popup_open');
 }
 
 const formSubmit = (event) => {
-    saveUserData()
+    event.preventDefault();
+    saveUserData();
     popupClose(event);
+
 }
 
 popupOpenButton.addEventListener('click', popupOpen) 
 popupCloseButton.addEventListener('click', popupClose)
-popupSaveButton.addEventListener('click', formSubmit);
+popupSaveButton.addEventListener('submit', formSubmit);
 
 
 
